fa5ed62499f8a75737dd62d7414e3a878f58ee6f
Lucas <lucas.roseira@fatec.sp.gov.br>
2020-09-06 23:29:44 -0300

    Até cursores

--- SQL/11_Loop_continue.sql
+++ SQL/11_Loop_continue.sql
@@ -5,7 +5,7 @@ DECLARE
       DBMS_OUTPUT.PUT_LINE ('Dentro do Loop o valor e igual a: '||TO_CHAR(P_VALOR));
       P_VALOR := P_VALOR + 1;
       CONTINUE WHEN P_VALOR < 5;
-      DBMS_OUTPUT.PUT_LINE ('Apos a cláusula CONTINUE, o valor e igual a: '||TO_CHAR(P_VALOR));
+      DBMS_OUTPUT.PUT_LINE ('Apos a clÃ¡usula CONTINUE, o valor e igual a: '||TO_CHAR(P_VALOR));
       EXIT WHEN P_VALOR = 5;
     END LOOP;
     DBMS_OUTPUT.PUT_LINE ('Apos o loop, o valor e igual a:' || TO_CHAR(P_VALOR));
@@ -17,15 +17,15 @@ DECLARE
   DECLARE
    valor NUMBER := 0;
   BEGIN
-    LOOP -- Após a declaração CONTINUE
-      DBMS_OUTPUT.PUT_LINE ('Valor apresentado no interior do loop é igual a: ' || TO_CHAR(valor));
+    LOOP -- ApÃ³s a declaraÃ§Ã£o CONTINUE
+      DBMS_OUTPUT.PUT_LINE ('Valor apresentado no interior do loop Ã© igual a: ' || TO_CHAR(valor));
       valor := valor + 1;
       IF valor < 15 THEN
         CONTINUE;
       END IF;
-      DBMS_OUTPUT.PUT_LINE ('No interior do loop, após a cláusula IF-CONTINUE, o valor é igual a: ' || TO_CHAR(valor));
+      DBMS_OUTPUT.PUT_LINE ('No interior do loop, apÃ³s a clÃ¡usula IF-CONTINUE, o valor Ã© igual a: ' || TO_CHAR(valor));
       EXIT WHEN valor > 15;
     END LOOP;
-     DBMS_OUTPUT.PUT_LINE ('Após o loop, o valor é igual a: ' || TO_CHAR(valor));
+     DBMS_OUTPUT.PUT_LINE ('ApÃ³s o loop, o valor Ã© igual a: ' || TO_CHAR(valor));
   END;
   /
